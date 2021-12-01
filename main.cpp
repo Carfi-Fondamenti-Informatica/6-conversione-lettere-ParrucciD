c12cfa1e1c199d08afc99169ba49d6938eb132b6
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-12-01 19:05:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char a;
+  cin>>a;
+  bool risultato =primo(a);
+  if(risultato){
+    cout << trasformaziona(a);
+  }else{
+    cout <<"errore"<< endl;
+  }
   return 0;
 }
